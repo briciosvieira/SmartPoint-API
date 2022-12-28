c295c0ed7bf28e4b7741fab86525fb268f045151
Fabricio Vieira <94842131+Briciosvieir@users.noreply.github.com>
2022-12-27 23:46:41 -0300

    Criação dos dados dos funcionarios

--- src/main/java/entity/Employee.java
+++ src/main/java/entity/Employee.java
@@ -1,10 +1,7 @@
 package entity;
 
 import enums.ProfileEnum;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
@@ -12,6 +9,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Entity
 @Data
@@ -22,17 +20,45 @@ public class Employee implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(length = 140, nullable = false)
     private String name;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
     private String cpf;
+    @Column(nullable = true, name = "hour_value")
     private BigDecimal hourValue;
+    @Column(name = "hours_worked_day")
     private Float hoursWorkedDay;
+    @Column(name = "lunch_time")
     private Float lunchTime;
+    @Column
     private ProfileEnum profile;
+    @Column(name = "created_date")
     private LocalDate createdDate;
+    @Column(name = "update_date")
     private Date updateDate;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "profile", nullable = false)
+    private ProfileEnum profileEnum;
+
+    @OneToMany(mappedBy = "employee", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Lauch>lauches;
 
+    @Transient
+    public Optional<BigDecimal> getHourValueOpt() {
+        return Optional.ofNullable(hourValue);
+    }
 
+    @Transient
+    public Optional<Float> getHoursWorkedDayOpt() {
+        return Optional.ofNullable(hoursWorkedDay) ;
+    }
 }
