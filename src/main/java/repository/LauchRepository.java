0ec2332a9184f45b7acf049a39f038055785f7f1
Fabricio Vieira <94842131+Briciosvieir@users.noreply.github.com>
2022-12-29 11:31:31 -0300

    Criando repositorio

---
+++ src/main/java/repository/LauchRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import entity.Lauch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LauchRepository extends JpaRepository<Lauch,Integer> {
+}
