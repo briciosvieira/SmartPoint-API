cc5b414db0e3db070e3122a54369b6f112a72973
Fabricio Vieira <94842131+Briciosvieir@users.noreply.github.com>
2023-01-17 18:46:36 -0300

    Iniciando teste Lançamento

--- src/test/java/com/studies/smartPoint/repositoryTests/LauchRepositoryTest.java
+++ src/test/java/com/studies/smartPoint/repositoryTests/LauchRepositoryTest.java
@@ -1,4 +1,34 @@
 package com.studies.smartPoint.repositoryTests;
 
+import com.studies.smartPoint.entity.Company;
+import com.studies.smartPoint.repository.CompanyRepository;
+import com.studies.smartPoint.repository.EmployeeRepository;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
 public class LauchRepositoryTest {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public LauchRepositoryTest() {
+    }
+
+    private static final String EMAIL = "email@test.com";
+    private static final String CPF = "12345678900";
+
+    @Before
+    public void setUp() throws Exception{
+        Company company = this.companyRepository.save(getCompanyData());
+        this.employeeRepository.save(getEmployeeData());
+    }
 }
