0f2003ca850609bae7595689e6ba91007b173576
Fabricio Vieira <94842131+Briciosvieir@users.noreply.github.com>
2023-01-20 23:03:39 -0300

    Criando teste de lançamento

--- src/test/java/com/studies/smartPoint/repositoryTests/LauchRepositoryTest.java
+++ src/test/java/com/studies/smartPoint/repositoryTests/LauchRepositoryTest.java
@@ -1,14 +1,34 @@
 package com.studies.smartPoint.repositoryTests;
 
+
 import com.studies.smartPoint.entity.Company;
+import com.studies.smartPoint.entity.Employee;
+import com.studies.smartPoint.entity.Lauch;
+import com.studies.smartPoint.enums.ProfileEnum;
+import com.studies.smartPoint.enums.TypeEnum;
 import com.studies.smartPoint.repository.CompanyRepository;
 import com.studies.smartPoint.repository.EmployeeRepository;
+import com.studies.smartPoint.repository.LauchRepository;
+import com.studies.smartPoint.utils.PasswordUtils;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,15 +40,64 @@ public class LauchRepositoryTest {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @Autowired
+    private LauchRepository lauchRepository;
+
+    private long employeeId;
+
     public LauchRepositoryTest() {
     }
-
-    private static final String EMAIL = "email@test.com";
-    private static final String CPF = "12345678900";
 
     @Before
     public void setUp() throws Exception{
         Company company = this.companyRepository.save(getCompanyData());
-        this.employeeRepository.save(getEmployeeData());
+
+        Employee employee = this.employeeRepository.save(getEmployeeData(company));
+        this.employeeId = employee.getId();
+    }
+
+    @After
+    public void tearDown()throws Exception{
+        this.companyRepository.deleteAll();
+    }
+
+    @Test
+    public void testGetLauchByEmployeeId(){
+        List<Lauch> lauches = this.lauchRepository.findByEmployeeId((int) employeeId);
+       assertEquals(2, lauches.size());
+    }
+
+    @Test
+    public void testGetLauchByEmployeePage(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.unsorted());
+        Page<Lauch> lauches = this.lauchRepository.findByEmployeeId((int) employeeId,pageable);
+        assertEquals(2, lauches.getTotalElements());
+    }
+
+    private Lauch getLauchData(Employee employee){
+        Lauch lauch = new Lauch();
+        lauch.setDate(new Date());
+        lauch.setType(TypeEnum.INICIO_PAUSA);
+        lauch.setEmployee(employee);
+        return lauch;
+    }
+
+    private Employee getEmployeeData(Company company) throws NoSuchAlgorithmException {
+        Employee employee = new Employee();
+        employee.setName("Fabricio");
+        employee.setProfileEnum(ProfileEnum.ROLE_USUARIO);
+        employee.setPassword(PasswordUtils.genetedBcrypt("123456"));
+        employee.setCpf("123.123.123.99");
+        employee.setEmail("fabricio@test.com");
+        employee.setCompany(company);
+        return employee;
+
+    }
+
+    private Company getCompanyData(){
+        Company company = new Company();
+        company.setCorporateName("Unidev");
+        company.setCnpj("51436645000100");
+        return company;
     }
 }
